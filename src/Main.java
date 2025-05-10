9e475fcad7dbb79d8211fc4d278f04ffa4a81489
YujiSoftware <yuji.software+github@gmail.com>
2025-05-11 00:40:54 +0900

    書いた

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,21 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Map<String, String> m = new HashMap<String, String>();
-        m.put("HND", "羽田空港");
-        m.put("NRT", "成田空港");
-        m.put("ASJ", "奄美空港");
+        HashMap<String, Integer> m = new HashMap<String, Integer>();
+        m.put("Java", 1996);
+        m.put("Kotlin", 2011);
+        m.put("Python", 1991);
+        m.put("Ruby", 1995);
+        m.put("PHP", 1995);
+        m.put("Go", 2011);
+        m.put("Rust", 2013);
+        m.put("Fortran", 1956);
+        m.put("C#", 2002);
+        m.put("Perl", 1987);
+        m.put("Scala", 2004);
+        m.put("C", 1972);
+        m.put("Swift", 2014);
 
-        System.out.println(m.get("HND"));
+        System.out.println(m.get("Java"));
     }
 }
