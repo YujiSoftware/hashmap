c9711e585ed7298771115fb3250d8e986102b5ab
YujiSoftware <yuji.software+github@gmail.com>
2025-05-11 20:56:01 +0900

    頑張った

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
-import java.util.Map;
-
 public class Main {
     public static void main(String[] args) {
         HashMap<String, Integer> m = new HashMap<String, Integer>();
-        m.put("Java", 1996);
+        m.put("Java", 1995);
+        m.put("JavaScript", 1995);
         m.put("Kotlin", 2011);
         m.put("Python", 1991);
         m.put("Ruby", 1995);
@@ -15,7 +14,6 @@ public class Main {
         m.put("Perl", 1987);
         m.put("Scala", 2004);
         m.put("C", 1972);
-        m.put("Swift", 2014);
 
         System.out.println(m.get("Java"));
     }
